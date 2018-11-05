092bbe29d7a4bcf520860298fede1d0b5f8464a4
andyka1714 <andyka1714@gmail.com>
2018-11-05 11:11:34 +0800

    add webview for bonny live

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, WebView } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <WebView source={{uri: 'https://www.bonny-badminton.tw/'}} style={styles.container} />
+    )
   }
 }
 
@@ -16,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
