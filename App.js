b3ad8ae2c5b437d6ec146fba4bcfe95ff7afa5e3
andyka1714 <andyka1714@gmail.com>
2018-11-19 11:18:46 +0800

    change url of webview

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { StyleSheet, WebView } from 'react-native'
+import { StyleSheet, WebView, View, Text, Button, Share } from 'react-native'
 
 export default class App extends React.Component {
+  testShare () {
+    Share.share(
+      {
+        url: 'https://www.ptt.cc/bbs/Soft_Job/M.1534771650.A.5D5.html'
+      })
+      .then(result => console.log(result))
+      .catch(errorMsg => console.log(errorMsg))
+  }
+
   render () {
     return (
-      <WebView source={{uri: 'https://www.bonny-badminton.tw/'}} style={styles.container} />
+      <View
+        style={{
+          flexDirection: 'row',
+          flex: 1
+        }}>
+        <WebView source={{uri: 'https://staging.bonnylive.biz/'}} style={styles.container} />
+        <View
+          style={{
+            position: 'absolute', left: 0, right: 0, bottom: 50, justifyContent: 'flex-end', alignItems: 'center'
+          }}>
+        >
+          <Button
+            onPress={this.testShare.bind(this)}
+            title='Share'
+            color='#fff'
+          />
+        </View>
+      </View>
     )
   }
 }
