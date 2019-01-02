c7be6e87b9386e1d1d266465a7cb376ae8336022
andyka1714 <andyka1714@gmail.com>
2019-01-02 14:33:21 +0800

    finish feature of phase 1

--- App.js
+++ App.js
@@ -1,39 +1,126 @@
 import React from 'react'
-import { StyleSheet, WebView, View, Button, Share } from 'react-native'
+import { View, Button, Alert, Text, StyleSheet, Share, WebView, BackHandler, StatusBar } from 'react-native'
+import createInvoke from 'react-native-webview-invoke/native'
+var Geolocation = require('Geolocation');
 
 export default class App extends React.Component {
-  testShare () {
-    Share.share(
-      {
-        url: 'https://www.ptt.cc/bbs/Soft_Job/M.1534771650.A.5D5.html'
-      })
-      .then(result => console.log(result))
-      .catch(errorMsg => console.log(errorMsg))
+  constructor(props) {
+    super(props);
+    this.state = { 
+      message: 'Get Geolocation',
+      location: {},
+      showBackButton: false
+    };
+  }
+  webview: WebView
+  invoke = createInvoke(() => this.webview)
+  // webInitialize = () => {
+    
+  // }
+  webWannaGet = () => {
+    return this.state.location;
+  }
+  webWannaSet = (data) => {
+    this.setState({ message: data })
   }
 
-  render () {
-    return (
-      <View
-        style={{
-          flexDirection: 'row',
-          flex: 1
-        }}>
-        <WebView source={{uri: 'https://www.bonny-badminton.tw/'}} style={styles.container} />
+  handleBackPress = () => {
+    this.webview.goBack();
+    return true  ;
+  }
+
+  navStateChange = (state) => {
+    let title = state.title;
+    let isInclue = title.includes('波力雲羽集')
+    this.setState({showBackButton: !isInclue})
+  }
+  
+  componentDidMount() {
+    this.invoke
+      .define('get', this.webWannaGet)
+      .define('set', this.webWannaSet)
+
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    this.getLocation();
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
+  }
+
+  //get geolocation
+  getLocation() {
+    Geolocation.getCurrentPosition(
+        location => {
+            var coords = {
+              longitude: location.coords.longitude,
+              latitude: location.coords.latitude
+            }
+            this.setState(previousState => (
+              { location: coords }
+            ))
+        },
+        error => {
+          Alert.alert("获取位置失败："+ error)
+        }
+    );
+ }
+
+
+  render() {
+    const backButton = <View
+    style={{
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      height: 35,
+      backgroundColor: '#c30d22',
+    }}>
+    <Button
+      onPress={this.handleBackPress}
+      title="< Back"
+      color="#ffffff"
+    />
+  </View>;
+
+    const patchPostMessageFunction = function() {
+        var originalPostMessage = window.postMessage;
+    
+        var patchedPostMessage = function(message, targetOrigin, transfer) {
+            originalPostMessage(message, targetOrigin, transfer);
+        };
+    
+        patchedPostMessage.toString = function() {
+            return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
+        };
+    
+        window.postMessage = patchedPostMessage;
+    };
+    
+    const patchPostMessageJsCode = '(' + String(patchPostMessageFunction) + ')();';
+      return (
         <View
           style={{
-            position: 'absolute', left: 0, right: 0, bottom: 50, justifyContent: 'flex-end', alignItems: 'center'
+            flex: 1
           }}>
-        >
-          <Button
-            onPress={this.testShare.bind(this)}
-            title='Share'
-            color='#fff'
+          <StatusBar
+            barStyle="light-content"
           />
+          <WebView
+            injectedJavaScript={patchPostMessageJsCode}
+            ref={w => this.webview = w}
+            source={{uri: 'https://www.bonny-badminton.tw/'}}
+            onMessage={this.invoke.listener}
+            style={styles.container}
+            onNavigationStateChange={this.navStateChange.bind(this)}
+            />
+          {this.state.showBackButton && backButton}
         </View>
-      </View>
-    )
+      )
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
